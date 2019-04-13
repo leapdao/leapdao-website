32a482195709cbaac1e46a860fb00c0f6014528e
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-13 16:26:21 +0100

    Fix:  automate frontpage metrics #33 (#43)
    * Fix:  automate frontpage metrics #33
    
    * Update changes
    
    * Change slack analytics endpoint
    
    * Update contributors count to match organization repos contributors

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
     { color: [255, 255, 0], radius: 120, x: 290, y: 330, dir: 1 }
   ];
 
-  const svgRoot = document.getElementById('amoebe');
-  const svgCircles = Array.from(svgRoot.querySelectorAll('circle'));
+  const svgRoot = document.getElementById("amoebe");
+  const svgCircles = Array.from(svgRoot.querySelectorAll("circle"));
 
   const speed = 1.1;
 
@@ -17,8 +17,8 @@
     svgCircle.setAttribute('cx', circle.x);
     svgCircle.setAttribute('cy', circle.y);
     svgCircle.setAttribute(
-      'fill',
-      `rgb(${circle.color.map(Math.round).join(',')})`
+      "fill",
+      `rgb(${circle.color.map(Math.round).join(",")})`
     );
   });
 
@@ -34,10 +34,10 @@
           circle.radius *
           circle.dir) /
         3;
-      svgCircles[i].setAttribute('transform', `translate(${x} ${y})`);
+      svgCircles[i].setAttribute("transform", `translate(${x} ${y})`);
       const rFn = i % 2 ? Math.cos : Math.sin;
       svgCircles[i].setAttribute(
-        'r',
+        "r",
         circle.radius + rFn((t / 500) * speed) * 5
       );
     });
@@ -67,12 +67,64 @@
     };
   };
 
+  const fetchContributors = () => {
+    const organizationRepositories = [];
+    fetch("https://api.github.com/orgs/leapdao/repos")
+      .then(response => response.json())
+      .then(repos => {
+        repos.map((repo, index) => {
+          fetch(`${repo.contributors_url}`)
+            .then(response => response.json())
+            .then(repositories => {
+              organizationRepositories.push(repositories);
+              const contributors = organizationRepositories.reduce(
+                (previousValue, currentValue) => {
+                  return previousValue + currentValue.length;
+                },
+                0
+              );
+              document.getElementById("contributors").innerHTML =
+                "<strong>" + contributors + "</strong> contributors";
+            });
+        });
+      });
+  };
+
+  const fetchMembers = () => {
+    fetch(`https://nplrpwwfw1.execute-api.eu-west-1.amazonaws.com/prod/slack`)
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById("members").innerHTML =
+          "<strong>" + data + "</strong> members";
+      });
+  };
+
+  const fetchUTXO = () => {
+    const url = window.location.pathname.split(".")[0];
+    if (url === "test") {
+      fetch("./mocks/testnet.json")
+        .then(response => response.json())
+        .then(
+          data => (document.getElementById("utxos").innerHTML = `~${data.data}`)
+        );
+    } else {
+      fetch("./mocks/mainnet.json")
+        .then(response => response.json())
+        .then(
+          data => (document.getElementById("utxos").innerHTML = `~${data.data}`)
+        );
+    }
+  };
+
   runWithFps(draw, 30);
+  fetchContributors();
+  fetchMembers();
+  fetchUTXO();
 })();
 
 (function() {
   function buildRequestUrl(form) {
-    const action = form.getAttribute('action');
+    const action = form.getAttribute("action");
     const formData = new FormData(form);
     const url = Array.from(formData.entries()).reduce(
       (memo, [key, value]) => `${memo}&${key}=${encodeURIComponent(value)}`,
@@ -82,38 +134,38 @@
   }
 
   function register2(form) {
-    const alertEl = form.querySelector('.alert');
-    const emailEl = form.querySelector('.email');
-    const submitEl = form.querySelector('.submit');
+    const alertEl = form.querySelector(".alert");
+    const emailEl = form.querySelector(".email");
+    const submitEl = form.querySelector(".submit");
 
     // jsonp
     const callbackName = `antiJQuery_${Date.now()}`;
     const url = `${buildRequestUrl(form)}&c=${callbackName}`;
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
+    const script = document.createElement("script");
+    script.type = "text/javascript";
     script.src = url;
     document.head.appendChild(script);
 
-    alertEl.innerText = 'Subscribing...';
-    emailEl.setAttribute('disabled', true);
-    submitEl.setAttribute('disabled', true);
+    alertEl.innerText = "Subscribing...";
+    emailEl.setAttribute("disabled", true);
+    submitEl.setAttribute("disabled", true);
 
     // callback for jsonp
     window[callbackName] = result => {
       try {
-        if (result.result !== 'success') {
+        if (result.result !== "success") {
           alertEl.innerHTML = result.msg;
-          emailEl.removeAttribute('disabled');
-          submitEl.removeAttribute('disabled');
-          emailEl.style.display = 'block';
-          submitEl.style.display = 'block';
+          emailEl.removeAttribute("disabled");
+          submitEl.removeAttribute("disabled");
+          emailEl.style.display = "block";
+          submitEl.style.display = "block";
         } else {
-          alertEl.innerHTML = 'Thank you!';
+          alertEl.innerHTML = "Thank you!";
         }
       } catch (err) {
-        alertEl.innerHTML = 'Ops, there was an error.';
-        emailEl.removeAttribute('disabled');
-        submitEl.removeAttribute('disabled');
+        alertEl.innerHTML = "Ops, there was an error.";
+        emailEl.removeAttribute("disabled");
+        submitEl.removeAttribute("disabled");
       }
 
       // cleaning up
@@ -122,75 +174,75 @@
     };
   }
 
-  const subscribeForm = document.getElementById('mc-embedded-subscribe-form');
+  const subscribeForm = document.getElementById("mc-embedded-subscribe-form");
   if (subscribeForm) {
-    subscribeForm.addEventListener('submit', e => {
+    subscribeForm.addEventListener("submit", e => {
       e.preventDefault();
       register2(e.target);
     });
   }
 
   if (window.onpopstate === null) {
-    const loader = document.querySelector('.loader');
+    const loader = document.querySelector(".loader");
     // browser supports onpopstate
     function loadPage(pathname) {
-      loader.style.display = 'block';
+      loader.style.display = "block";
       const xhr = new XMLHttpRequest();
-      xhr.open('get', pathname);
+      xhr.open("get", pathname);
       xhr.onreadystatechange = () => {
         if (xhr.readyState === xhr.DONE) {
           const html = xhr.responseText;
-          const dom = new DOMParser().parseFromString(html, 'text/html');
-          const targetPage = document.querySelector('.page');
-          const sourcePage = dom.querySelector('.page');
+          const dom = new DOMParser().parseFromString(html, "text/html");
+          const targetPage = document.querySelector(".page");
+          const sourcePage = dom.querySelector(".page");
           targetPage.innerHTML = sourcePage.innerHTML;
-          targetPage.setAttribute('class', sourcePage.getAttribute('class'));
-          document.querySelector('title').innerHTML = dom.querySelector(
-            'title'
+          targetPage.setAttribute("class", sourcePage.getAttribute("class"));
+          document.querySelector("title").innerHTML = dom.querySelector(
+            "title"
           ).innerHTML;
-          const scripts = Array.from(sourcePage.querySelectorAll('script'))
+          const scripts = Array.from(sourcePage.querySelectorAll("script"))
             .filter(s => !s.src)
             .map(s => s.innerHTML);
           if (scripts) {
-            const scriptTag = document.createElement('script');
-            scriptTag.innerHTML = scripts.join(';\n');
+            const scriptTag = document.createElement("script");
+            scriptTag.innerHTML = scripts.join(";\n");
             targetPage.appendChild(scriptTag);
           }
           window.scrollTo(0, 0);
 
           Array.from(
-            document.querySelectorAll('head script.page-script')
+            document.querySelectorAll("head script.page-script")
           ).forEach(el => {
             document.head.removeChild(el);
           });
-          Array.from(dom.querySelectorAll('.page script')).forEach(
+          Array.from(dom.querySelectorAll(".page script")).forEach(
             oldScript => {
-              const script = document.createElement('script');
+              const script = document.createElement("script");
               script.src = oldScript.src;
-              script.className = 'page-script';
+              script.className = "page-script";
               script.async = false;
               document.head.appendChild(script);
             }
           );
 
-          loader.style.display = 'none';
+          loader.style.display = "none";
         }
       };
       xhr.send();
     }
-    document.addEventListener('click', e => {
+    document.addEventListener("click", e => {
       if (
-        e.target.tagName === 'A' &&
-        e.target.getAttribute('href').startsWith('/') &&
+        e.target.tagName === "A" &&
+        e.target.getAttribute("href").startsWith("/") &&
         e.which === 1 &&
         !e.metaKey
       ) {
         e.preventDefault();
-        loadPage(e.target.getAttribute('href'));
-        window.history.pushState(null, null, e.target.getAttribute('href'));
+        loadPage(e.target.getAttribute("href"));
+        window.history.pushState(null, null, e.target.getAttribute("href"));
       }
     });
-    window.addEventListener('popstate', e => {
+    window.addEventListener("popstate", e => {
       loadPage(window.location.pathname);
     });
   }
