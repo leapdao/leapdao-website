a8065633f3c032c197926509b8cc992992451212
Eswara Sai <eswarasai.v@gmail.com>
2019-11-24 20:03:55 +0530

    Refactored to render variable in Svelte

--- src/routes/_index/Stats.svelte
+++ src/routes/_index/Stats.svelte
@@ -1,6 +1,10 @@
 <script>
   import { onMount } from "svelte";
 
+  let contributors = 0;
+  let members = 0;
+  let utxos = 0;
+
   const fetchContributors = () => {
     fetch("https://api.github.com/orgs/leapdao/repos")
       .then(response => response.json())
@@ -23,35 +27,20 @@
             )
           )
       )
-      .then(allContributors => {
-        const contributors = document.getElementById("contributors");
-        if (contributors) {
-          contributors.innerHTML = allContributors.size;
-        }
-      })
+      .then(allContributors => contributors = allContributors.size)
       .catch(() => {});
   };
 
   const fetchMembers = () => {
     fetch(`https://nplrpwwfw1.execute-api.eu-west-1.amazonaws.com/prod/slack`)
       .then(response => response.json())
-      .then(data => {
-        const members = document.getElementById("members");
-        if (members) {
-          members.innerHTML = data;
-        }
-      });
+      .then(data => members = data);
   };
 
   const fetchUTXO = () => {
     fetch(`https://n70fngzq9b.execute-api.us-east-1.amazonaws.com/dev/stats/get`)
       .then(response => response.json())
-      .then(data => {
-        const utxos = document.getElementById("utxos");
-        if (utxos) {
-            utxos.innerHTML = data.count;
-        }
-      });
+      .then(data => utxos = data.count);
   };
 
   onMount(() => {
@@ -75,15 +64,15 @@
 <div class="stats">
   <div class="stats-repos">
     <div class="stats-repo">
-      <strong id="utxos" class="stats-repo-data" />
+      <strong id="utxos" class="stats-repo-data">{utxos}</strong>
       <span> monthly active UTXOs</span>
     </div>
     <div class="stats-repo">
-      <strong id="members" class="stats-repo-data" />
+      <strong id="members" class="stats-repo-data">{members}</strong>
       <span>&nbsp;members</span>
     </div>
     <div class="stats-repo">
-      <strong id="contributors" class="stats-repo-data" />
+      <strong id="contributors" class="stats-repo-data">{contributors}</strong>
       <span>&nbsp;contributors</span>
     </div>
   </div>
