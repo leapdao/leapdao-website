0026ae79cedca81308d03fd9b90fe6aa601fbe43
Kosta Korenkov <kosta@whoot.me>
2019-12-11 14:08:08 +0300

    chore: fix formatting

--- src/routes/earn.svelte
+++ src/routes/earn.svelte
@@ -174,7 +174,10 @@
 <div class="content">
   <header>
     <Logo />
-    <p>Earn with LeapDAO by completing a bounty, taking on a role, or applying for a grant</p>
+    <p>
+      Earn with LeapDAO by completing a bounty, taking on a role, or applying
+      for a grant
+    </p>
   </header>
 
   <h2>Grants</h2>
@@ -203,76 +206,76 @@
     </div>
   </div>
 
-  <h2>Roles</h2>	
-  <div class="cols">	
-    <div class="col">	
-      <p>	
-        LeapDAO is an open community operating on	
-        <a href="https://www.holacracy.org/" target="_blank">Holacracy</a>	
-        – a non-hierarchical governance methodology. There are no job positions,	
-        but roles, and every partner holds one or more of them. Some roles may	
-        be vacant from time to time, so if you'd like to join our community and	
-        take a role listed below – join our	
-        <a	
-          href="https://docs.google.com/forms/d/e/1FAIpQLSd8_wDGDAi__HvfYEWNK_bvJzIkxwHHRVL6AFEfJewBd2Vn9A/viewform">	
-          Slack	
-        </a>	
-        and express your intention.	
-        <br />	
-        View our organization	
-        <a href="https://app.glassfrog.com/organizations/14849/roles/10883348">	
-          Glassfrog	
-        </a>	
-        .	
-      </p>	
-    </div>	
-    <div class="col">	
-      <p>	
-        <a	
-          class="nohighlight"	
-          href="https://app.glassfrog.com/organizations/14849/roles/10883348">	
-          <img	
-            src="/img/glassfrog-circles.png"	
-            width="300"	
-            alt="Glassfrog"	
-            title="Glassfrog" />	
-        </a>	
-      </p>	
-    </div>	
-  </div>	
-  <div id="roles">	
-    {#if rolesData}	
-      {#each rolesData.roles as role}	
-        <div>	
-          <p>	
-            <span class="glassfrog" />	
-            <big>{role.name_with_circle_for_core_roles}</big>	
-            <br />	
-            <small>	
-              <strong>Purpose:</strong>	
-              {role.purpose}	
-              <br />	
-              <strong>Accountabilities:</strong>	
-            </small>	
-          </p>	
-          <ul>	
-            {#each role.accountabilities as accountability}	
-              <li>{accountability.description}</li>	
-            {/each}	
-          </ul>	
-          <p class="compact">	
-            <small>	
-              <strong>Domains:</strong>	
-            </small>	
-            {#each role.domains as domain}	
-              <span class="tag">{domain.description}</span>	
-            {/each}	
-          </p>	
-        </div>	
-      {/each}	
-    {:else}	
-      <p>Loading...</p>	
-    {/if}	
+  <h2>Roles</h2>
+  <div class="cols">
+    <div class="col">
+      <p>
+        LeapDAO is an open community operating on
+        <a href="https://www.holacracy.org/" target="_blank">Holacracy</a>
+        – a non-hierarchical governance methodology. There are no job positions,
+        but roles, and every partner holds one or more of them. Some roles may
+        be vacant from time to time, so if you'd like to join our community and
+        take a role listed below – join our
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSd8_wDGDAi__HvfYEWNK_bvJzIkxwHHRVL6AFEfJewBd2Vn9A/viewform">
+          Slack
+        </a>
+        and express your intention.
+        <br />
+        View our organization
+        <a href="https://app.glassfrog.com/organizations/14849/roles/10883348">
+          Glassfrog
+        </a>
+        .
+      </p>
+    </div>
+    <div class="col">
+      <p>
+        <a
+          class="nohighlight"
+          href="https://app.glassfrog.com/organizations/14849/roles/10883348">
+          <img
+            src="/img/glassfrog-circles.png"
+            width="300"
+            alt="Glassfrog"
+            title="Glassfrog" />
+        </a>
+      </p>
+    </div>
+  </div>
+  <div id="roles">
+    {#if rolesData}
+      {#each rolesData.roles as role}
+        <div>
+          <p>
+            <span class="glassfrog" />
+            <big>{role.name_with_circle_for_core_roles}</big>
+            <br />
+            <small>
+              <strong>Purpose:</strong>
+              {role.purpose}
+              <br />
+              <strong>Accountabilities:</strong>
+            </small>
+          </p>
+          <ul>
+            {#each role.accountabilities as accountability}
+              <li>{accountability.description}</li>
+            {/each}
+          </ul>
+          <p class="compact">
+            <small>
+              <strong>Domains:</strong>
+            </small>
+            {#each role.domains as domain}
+              <span class="tag">{domain.description}</span>
+            {/each}
+          </p>
+        </div>
+      {/each}
+    {:else}
+      <p>Loading...</p>
+    {/if}
   </div>
 
   <h2>Bounties</h2>
