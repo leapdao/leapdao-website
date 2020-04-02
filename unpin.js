deacd6966c2b8036bd8ace408915bbd305100a78
Clearwood <Clearwood@users.noreply.github.com>
2020-04-02 18:41:38 +0200

    Update unpin.js
    Co-Authored-By: Kosta Korenkov <kosta@whoot.me>

--- unpin.js
+++ unpin.js
@@ -1,44 +1,25 @@
 const axios = require('axios');
 
+const options = {
+  headers: {
+    'pinata_api_key': process.env.IPFS_DEPLOY_PINATA__API_KEY,
+    'pinata_secret_api_key': process.env.IPFS_DEPLOY_PINATA__SECRET_API_KEY
+  }
+};
+
 function deletePin(hashToUnpin){
   const url = `https://api.pinata.cloud/pinning/unpin/${hashToUnpin}`;
-    return axios
-        .delete(
-            url,
-            {
-                headers: {
-                    'pinata_api_key': process.env.IPFS_DEPLOY_PINATA__API_KEY,
-                    'pinata_secret_api_key': process.env.IPFS_DEPLOY_PINATA__SECRET_API_KEY
-                }
-            }
-        ).then(function (response) {
-          //console.log(response);
-            //handle response here
-        })
-        .catch(function (error) {
-            //handle error here
-        });
+  return axios.delete(url, options);
 }
 
 function unPinExceptLast() {
-    const url = `https://api.pinata.cloud/data/pinList?status=pinned&pageOffset=1`;
-    return axios
-        .get(url, {
-            headers: {
-                'pinata_api_key': process.env.IPFS_DEPLOY_PINATA__API_KEY,
-                'pinata_secret_api_key': process.env.IPFS_DEPLOY_PINATA__SECRET_API_KEY
-            }
-        })
-        .then(function (response) {
-          response.data.rows.forEach(function(item){
-            deletePin(item.ipfs_pin_hash)
-          });
-          //console.log(response.data.rows);
-            //handle response here
-        })
-        .catch(function (error) {
-            //handle error here
-        });
+  const url = `https://api.pinata.cloud/data/pinList?status=pinned&pageOffset=1`;
+  return axios.get(url, options)
+    .then(function (response) {
+      response.data.rows.forEach(function(item){
+        deletePin(item.ipfs_pin_hash)
+      });
+    });
 }
 
 unPinExceptLast();
