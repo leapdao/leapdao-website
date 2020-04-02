a061d2beafb3e952152bddb0fdc38e0e7acbe044
Keno Budde <kenobudde@yahoo.de>
2020-04-02 19:08:53 +0200

    removed cloudfront

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./scripts/update-route53.sh
-aws s3 sync ./__sapper__/export s3://$PROD_S3_BUCKET/ --acl public-read
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $PROD_CLOUDFRONT_DISTRIBUTION --paths "/*"
+#aws s3 sync ./__sapper__/export s3://$PROD_S3_BUCKET/ --acl public-read
+#aws configure set preview.cloudfront true
+#aws cloudfront create-invalidation --distribution-id $PROD_CLOUDFRONT_DISTRIBUTION --paths "/*"
